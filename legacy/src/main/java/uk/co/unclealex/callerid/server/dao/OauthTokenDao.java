c5db8e7277dc0fde775fcb4cd84b3bd585e90a91
Alex Jones <alex.jones@unclealex.co.uk>
2012-07-30 19:24:37 +0100

    Removed some legacy code.

--- legacy/src/main/java/uk/co/unclealex/callerid/server/dao/OauthTokenDao.java
+++
@@ -1,8 +0,0 @@
-package uk.co.unclealex.callerid.server.dao;
-
-import uk.co.unclealex.callerid.server.model.OauthToken;
-import uk.co.unclealex.hibernate.dao.KeyedDao;
-
-public interface OauthTokenDao extends KeyedDao<OauthToken> {
-	
-}
--- legacy/src/main/java/uk/co/unclealex/callerid/server/service/TelephoneNumberFactory.java
+++
@@ -1,8 +0,0 @@
-package uk.co.unclealex.callerid.server.service;
-
-import uk.co.unclealex.callerid.server.model.TelephoneNumber;
-
-public interface TelephoneNumberFactory {
-
-	public TelephoneNumber findOrCreateTelephoneNumber(String number);
-}
