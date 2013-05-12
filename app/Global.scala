9919b86b1b901efddb764b932a2bbc76236a92da
Alex Jones <alex.jones@unclealex.co.uk>
2013-05-12 21:41:35 +0100

    Add rudimentary Google OAuth authentication.

--- app/Global.scala
+++ app/Global.scala
@@ -29,11 +29,69 @@
 import play.api.GlobalSettings
 import module.DefaultModule
 import com.google.inject.Guice
-
+import org.squeryl.adapters.{ H2Adapter, PostgreSqlAdapter }
+import org.squeryl.internals.DatabaseAdapter
+import org.squeryl.{ Session, SessionFactory }
+import play.api.db.DB
+import play.api.GlobalSettings
+import uk.co.unclealex.callerid.remote.model.CallerIdSchema._
+import uk.co.unclealex.callerid.remote.model.CallerIdSchema
+import play.api.Application
+import org.pac4j.oauth.client.Google2Client
+import org.pac4j.http.client.BasicAuthClient
+import org.pac4j.http.credentials.UsernamePasswordAuthenticator
+import org.pac4j.http.credentials.UsernamePasswordCredentials
+import org.pac4j.core.exception.CredentialsException
+import org.pac4j.core.client.Clients
+import org.pac4j.play.Config
 object Global extends GlobalSettings {
+
+  // Guice
   private lazy val injector = Guice.createInjector(new DefaultModule)
 
+  // Guice
   override def getControllerInstance[A](clazz: Class[A]) = {
     injector.getInstance(clazz)
   }
+
+  override def onStart(app: Application) {
+
+    def propertyExtractor: String => String => String =
+      prefix => key => {
+        val fullKey = s"$prefix.$key"
+        app.configuration.getString(fullKey).getOrElse(
+          throw new IllegalArgumentException(s"Cannot find configuration option $fullKey"))
+      }
+
+    // Security
+    val googleClient = {
+      def googleProperty = propertyExtractor("google")
+      new Google2Client(googleProperty("consumerId"), googleProperty("consumerSecret"))
+    }
+    val basicHttpClient = {
+      def basicHttpProperty = propertyExtractor("security")
+      val username = basicHttpProperty("username")
+      val password = basicHttpProperty("password")
+      val authenticator = new UsernamePasswordAuthenticator() {
+        def validate(credentials: UsernamePasswordCredentials) = {
+          if (credentials.getUsername() != username || credentials.getPassword() != password) {
+            throw new CredentialsException("Please go away.")
+          }
+        }
+      }
+      new BasicAuthClient(authenticator)
+    }
+    val clients = new Clients(propertyExtractor("google")("callback"), basicHttpClient, googleClient)
+    Config.setClients(clients)
+
+    // Set up Squeryl database access
+    SessionFactory.concreteFactory = app.configuration.getString("db.default.driver") match {
+      case Some("org.h2.Driver") => Some(() => getSession(new H2Adapter, app))
+      case Some("org.postgresql.Driver") => Some(() => getSession(new PostgreSqlAdapter, app))
+      case _ => sys.error("Database driver must be either org.h2.Driver or org.postgresql.Driver")
+    }
+  }
+
+  def getSession(adapter: DatabaseAdapter, app: Application) = Session.create(DB.getConnection()(app), adapter)
+
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,11 @@ import play.api.mvc.Request
 import play.api.mvc.Result
 import uk.co.unclealex.callerid.remote.call.ReceivedCallsService
 import uk.co.unclealex.callerid.remote.number.NumberFormatter
+import uk.co.unclealex.callerid.remote.model.CallerIdSchema._
+import uk.co.unclealex.callerid.remote.model.CallerIdSchema
+import org.pac4j.play.scala.ScalaController
+import org.pac4j.play.CallbackController
+import play.mvc.Http.Context
 
 class Application @Inject() (
   /**
@@ -17,22 +22,25 @@ class Application @Inject() (
   /**
    * The received calls service used to list all received calls.
    */
-  receivedCallsService: ReceivedCallsService) extends Controller {
+  receivedCallsService: ReceivedCallsService) extends ScalaController {
 
-  def index = Transactional {
-    Action {
-      val allCallModels = receivedCallsService.calls.map(
-        rc => {
-          val pn = rc.phoneNumber
-          CallModel(rc, numberFormatter.formatNumberAsInternational(pn), numberFormatter.formatAddress(pn))
-        })
-      Ok(views.html.index(allCallModels))
-    }
+  def index = RequiresAuthentication("Google2Client") {
+    profile =>
+      Transactional {
+        Action {
+          val allCallModels = receivedCallsService.calls.map(
+            rc => {
+              val pn = rc.phoneNumber
+              CallModel(rc, numberFormatter.formatNumberAsInternational(pn), numberFormatter.formatAddress(pn))
+            })
+          Ok(views.html.index(allCallModels, profile.getDisplayName()))
+        }
+      }
   }
 
   case class Transactional[A](action: Action[A]) extends Action[A] {
 
-    def apply(request: Request[A]): Result = {
+    def apply(request: Request[A]): Result = inTransaction {
       action(request)
     }
 
