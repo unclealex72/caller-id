82eb524361e88ff3981eeac3f6c87dfaa217bea7
Alex Jones <alex.jones@unclealex.co.uk>
2018-05-28 18:28:09 +0100

    Shorten logging of phone calls.

--- app/notify/sinks/LoggingSink.scala
+++ app/notify/sinks/LoggingSink.scala
@@ -1,10 +1,17 @@
 package notify.sinks
 
-import call.Call
+import call._
 import com.typesafe.scalalogging.StrictLogging
+import number.NumberFormatter
 
-object LoggingSink extends StrictLogging with (Call => Unit) {
+class LoggingSink(numberFormatter: NumberFormatter) extends StrictLogging with (Call => Unit) {
   override def apply(call: Call): Unit = {
-    logger.info(s"Received call $call")
+    val formattedCaller: String = call.caller match {
+      case Withheld => "Withheld"
+      case Known(name, _, number) => s"$name on ${numberFormatter.formatNumber(number).default}"
+      case Unknown(number) => numberFormatter.formatNumber(number).default
+      case Undefinable(str) => s"unparseable $str"
+    }
+    logger.info(s"Received call from $formattedCaller at ${call.when}")
   }
 }
