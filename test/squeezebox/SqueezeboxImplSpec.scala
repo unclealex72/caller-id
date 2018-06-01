8faaa7e6e05e588f978bb5ffc0b746a48f6a2357
Alex Jones <alexander.jones@virginmedia.co.uk>
2018-06-01 12:01:25 +0100

    Squeezebox test now works.

--- test/squeezebox/SqueezeboxImplSpec.scala
+++ test/squeezebox/SqueezeboxImplSpec.scala
@@ -2,62 +2,105 @@ package squeezebox
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Source}
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream._
+import akka.stream.scaladsl.{BidiFlow, Flow}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 import akka.util.ByteString
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.StrictLogging
 import org.scalatest.{AsyncWordSpec, Matchers}
 
-import scala.collection.mutable
 import scala.concurrent.{Future, Promise}
+import scala.util.Success
 
 class SqueezeboxImplSpec extends AsyncWordSpec with Matchers with StrictLogging {
 
   implicit val actorSystem: ActorSystem = ActorSystem("notifierSpec", ConfigFactory.empty())
   implicit val materializer: Materializer = ActorMaterializer()
 
-  val eventualResultPromise: Promise[Seq[String]] = Promise[Seq[String]]
-  var requests: mutable.Seq[String] = mutable.Buffer[String]()
-
-  val serverFlow: Flow[ByteString, ByteString, NotUsed] = {
-    val id0 = "00%3A00"
-    val id1 = "01%3A01"
-
-    val serverResponses: Map[String, String] = Map(
-      "player count ?" -> "player count 2",
-      "player id 0 ?" -> s"player id 0 $id0",
-      "player id 1 ?" -> s"player id 1 $id1",
-      s"$id0 display Hello Hello 30" -> s"$id0 display Hello Hello 30",
-      s"$id1 display Hello Hello 30" -> s"$id1 display Hello Hello 30"
-    )
-
-    val toUtf8Flow: Flow[ByteString, String, NotUsed] = Flow.fromFunction { bytes =>
-      bytes.utf8String.replaceAll("\n", "")
-    }
-    val logicFlow: Flow[String, String, NotUsed] = Flow.fromFunction { request =>
-      logger.debug(s"Media server received request: $request")
-      serverResponses.get(request) match {
-        case Some(response) =>
-          logger.debug(s"Media server sending response: $response")
-          response
-        case None =>
-          logger.warn(s"Media server did not understand request: $request")
-          request
-      }
-    }
-    val fromUtf8Flow: Flow[String, ByteString, NotUsed] = Flow.fromFunction { str =>
-      ByteString(s"$str\n")
-    }
-    toUtf8Flow.via(logicFlow).via(fromUtf8Flow)
-  }.merge(Source.fromFuture(Future { Thread.sleep(3600 * 1000); ByteString("")}))
-
   "sending a request to display a message on all squeezeboxes" should {
     "elicit the correct responses from the media server" in {
-      val squeezeboxImpl = new SqueezeboxImpl(serverFlow, 30)
+      val squeezeboxImpl: SqueezeboxImpl[Future[Seq[String]]] = new SqueezeboxImpl(generateFlow(), 30)
       logger.info("Starting test")
-      squeezeboxImpl.display("Hello")
-      1 should === (1)
+      squeezeboxImpl.display("Hello") map { responses =>
+        responses should contain theSameElementsInOrderAs Seq(
+          "player count ?",
+          "player id 0 ?",
+          "00%3A00 display Hello Hello 30",
+          "player id 1 ?",
+          "01%3A01 display Hello Hello 30",
+          "exit")
+      }
     }
+  }
+
+  def generateFlow(): Flow[ByteString, ByteString, Future[Seq[String]]] = {
+    val responsesPromise: Promise[Seq[String]] = Promise()
+    class Logic extends GraphStage[FlowShape[String, String]] {
+      val in: Inlet[String] = Inlet("logic.in")
+      val out: Outlet[String] = Outlet("logic.out")
+
+      override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = {
+        new GraphStageLogic(shape) {
+
+          val id0 = "00%3A00"
+          val id1 = "01%3A01"
+
+          val serverResponses: Map[String, String] = Map(
+            "player count ?" -> "player count 2",
+            "player id 0 ?" -> s"player id 0 $id0",
+            "player id 1 ?" -> s"player id 1 $id1",
+            s"$id0 display Hello Hello 30" -> s"$id0 display Hello Hello 30",
+            s"$id1 display Hello Hello 30" -> s"$id1 display Hello Hello 30"
+          )
+
+          var requests: Seq[String] = Seq.empty
+          var maybeNextResponse: Option[String] = None
+
+          setHandlers(in, out, new InHandler with OutHandler {
+            override def onPush(): Unit = {
+              val request: String = grab(in)
+              logger.debug(s"Media server received request: $request")
+              requests = requests :+ request
+              if (request == "exit") {
+                responsesPromise.complete(Success(requests))
+                completeStage()
+              }
+              else {
+                val nextResponse = serverResponses.get(request) match {
+                  case Some(response) =>
+                    logger.debug(s"Media server sending response: $response")
+                    response
+                  case None =>
+                    logger.warn(s"Media server did not understand request: $request")
+                    request
+                }
+                if (isAvailable(out)) {
+                  push(out, nextResponse)
+                  maybeNextResponse = None
+                  pull(in)
+                }
+                else {
+                  maybeNextResponse = Some(nextResponse)
+                }
+              }
+            }
+
+            override def onPull(): Unit = {
+              maybeNextResponse.foreach(push(out, _))
+              pull(in)
+            }
+          })
+        }
+      }
+
+      override def shape: FlowShape[String, String] = FlowShape(in, out)
+    }
+
+    val codec: BidiFlow[ByteString, String, String, ByteString, NotUsed] = BidiFlow.fromFunctions(
+      outbound = { bytes => bytes.utf8String.replaceAll("\n", "") },
+      inbound = { str => ByteString(s"$str\n") }
+    )
+    codec.join(new Logic()).mapMaterializedValue(_ => responsesPromise.future)
   }
 }
