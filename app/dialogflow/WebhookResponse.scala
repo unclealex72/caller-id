1ca5ad531314736057eca34a9df5fc400676dd81
Alex Jones <alex.jones@unclealex.co.uk>
2018-06-18 18:08:20 +0100

    Call responses are now concatenated into one webhook response message.

--- app/dialogflow/WebhookResponse.scala
+++ app/dialogflow/WebhookResponse.scala
@@ -9,10 +9,15 @@ object WebhookResponse {
   import play.api.libs.json._
 
   implicit val webhookResponseWrites : OWrites[WebhookResponse] = (webhookResponse: WebhookResponse) => {
-    val messages: NonEmptyList[JsObject] = webhookResponse.messages.map { message =>
-      Json.obj("text" -> Json.obj("text" -> JsArray(Seq(JsString(message)))))
+    val messages: NonEmptyList[String] = webhookResponse.messages
+    val (init: List[String], last: String) = messages.tail.foldLeft((List.empty[String], messages.head)) { (acc, message) =>
+      val (init, last) = acc
+      (init :+ last, message)
     }
-    val fulfillmentMessages = JsArray(messages.toList)
-    Json.obj("fulfillmentMessages" -> fulfillmentMessages, "outputContexts" -> JsArray())
+    val fulfillmentText: String = init match {
+      case Nil => last
+      case list => Seq(list.mkString(",\n"), last).mkString(" and\n")
+    }
+    Json.obj("fulfillmentText" -> JsString(fulfillmentText), "outputContexts" -> JsArray())
   }
 }
--- test/dialogflow/WebhookResponseSpec.scala
+++ test/dialogflow/WebhookResponseSpec.scala
@@ -10,9 +10,45 @@ import scala.util.Try
 
 class WebhookResponseSpec extends WordSpec with Matchers with StrictLogging {
 
-  "Serialising a webhook response" should {
-    "serialise correctly" in {
-      Right(Json.toJson(WebhookResponse(NonEmptyList.of("Somebody just called.", "But it wasn't me.")))) should === ("webhook-response.json".parsed())
+  "Serialising a webhook response for one call" should {
+    "serialise with no joining characters" in {
+      Right(Json.toJson(
+        WebhookResponse(
+          NonEmptyList.of(
+            "First call")))) should === ("single-webhook-response.json".parsed())
+    }
+  }
+
+  "Serialising a webhook response for two calls" should {
+    "serialise and join the two with 'and'" in {
+      Right(Json.toJson(
+        WebhookResponse(
+          NonEmptyList.of(
+            "First call",
+            "Second call")))) should === ("double-webhook-response.json".parsed())
+    }
+  }
+
+  "Serialising a webhook response for three calls" should {
+    "serialise and join the three with a comma and 'and'" in {
+      Right(Json.toJson(
+        WebhookResponse(
+          NonEmptyList.of(
+            "First call",
+            "Second call",
+            "Third call")))) should === ("triple-webhook-response.json".parsed())
+    }
+  }
+
+  "Serialising a webhook response for four calls" should {
+    "serialise and join the four with a comma and 'and'" in {
+      Right(Json.toJson(
+        WebhookResponse(
+          NonEmptyList.of(
+            "First call",
+            "Second call",
+            "Third call",
+            "Fourth call")))) should === ("quadruple-webhook-response.json".parsed())
     }
   }
 
